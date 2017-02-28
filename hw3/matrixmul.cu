3ceff4b289cba0fe02674f8131b7fc6fdfa1b7df
ice-tea <tmacback@gmail.com>
2017-02-28 18:34:35 -0500

    not output

--- hw3/matrixmul.cu
+++ hw3/matrixmul.cu
@@ -116,17 +116,17 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	OutPut(&M);
-	OutPut(&N);
+	//OutPut(&M);
+	//OutPut(&N);
 	// M * N on the device
     MatrixMulOnDevice(M, N, P);
-    OutPut(&P);
+    //OutPut(&P);
     
 	printf("GPU computation complete\n");
     // compute the matrix multiplication on the CPU for comparison
     Matrix reference = AllocateMatrix(P.height, P.width, 0);
     computeGold(reference.elements, M.elements, N.elements, M.height, M.width, N.width);
-    OutPut(&reference);
+    //OutPut(&reference);
 
 	printf("CPU computation complete\n");
     // in this case check if the result is equivalent to the expected soluion
